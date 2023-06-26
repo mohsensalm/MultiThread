17b150be03c30567155762206381e2293ff2919f
Pc <Pc@DESKTOP-PRAMDG2>
2023-06-26 11:30:08 +0330

    fix

---
+++ async and sync/AwaitingTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace async_and_sync
+{
+    public class AwaitingTest
+    {
+        public  async Task AwaitSequencially()
+        {
+            var watch = new Stopwatch();
+
+            Console.WriteLine("Starting sequencially async calls");
+
+            watch.Start();
+
+            await wait5sec();
+            await wait5sec_2();
+            await wait5sec_3();
+
+            watch.Stop();
+
+            Console.WriteLine($"finished sequencially async calls in {watch.ElapsedMilliseconds}");
+        }
+
+        public async Task AwaitInParallel()
+        {
+            var watch = new Stopwatch();
+
+            Console.WriteLine("Starting parallel async calls");
+
+            watch.Start();
+
+            var a = wait5sec();
+            var b = wait5sec_2();
+            var c = wait5sec_3();
+
+            Console.WriteLine("Doing other stuff");
+
+            await a;
+            await b;
+            await c;
+
+            watch.Stop();
+
+            Console.WriteLine($"finished parallel async calls in {watch.ElapsedMilliseconds}");
+        }
+
+        private async Task wait5sec() => await Task.Delay(5000);
+        private async Task wait5sec_2() => await Task.Delay(5000);
+        private async Task wait5sec_3() => await Task.Delay(5000);
+
+       
+
+    }
+
+}
---
+++ async and sync/Program.cs
@@ -0,0 +1,19 @@
+﻿using async_and_sync;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var test = new AwaitingTest();
+
+        Console.WriteLine("Starting app");
+
+        test.AwaitSequencially().Wait();
+        test.AwaitInParallel().Wait();
+
+        Console.WriteLine("Ending app");
+
+
+    }
+
+}
