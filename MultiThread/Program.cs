6f54d4971989f74d3baf7c6ad0b6e5609b3fa041
Pc <Pc@DESKTOP-PRAMDG2>
2023-06-26 10:08:37 +0330

    Add project files.

---
+++ MultiThread/Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// C# program to illustrate the
+
+// concept of Multithreading
+
+
+public class mainthread
+{
+
+    // static method one
+
+    public static void thrd1()
+
+    {
+
+        // It prints numbers from 0 to 10
+
+        for (int x = 0; x <= 10; x++)
+        {
+
+            Console.WriteLine("Thread1 is : {0}", x);
+
+            // When the x's value is equal to 5 then
+
+            // this method sleeps for 5 seconds
+
+            if (x == 5)
+            {
+
+                Thread.Sleep(5000);
+
+            }
+
+        }
+
+    }
+
+    // static method two
+
+    public static void thrd2()
+
+    {
+
+        // It prints numbers from 0 to 10
+
+        for (int y = 0; y <= 10; y++)
+        {
+
+            Console.WriteLine("Thread2 is : {0}", y);
+
+        }
+
+    }
+
+    // Main Method
+
+    static public void Main()
+
+    {
+
+        // Creating and initializing threads
+
+        Thread t1 = new Thread(thrd1);
+
+        Thread t2 = new Thread(thrd2);
+
+        t1.Start();
+
+        t2.Start();
+
+    }
+
+}
---
+++ Singlethread/Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+using System.Threading;
+
+namespace Threaddemo
+
+{
+
+    public class mainthread1
+    {
+
+        // static method one
+
+        public static void thrd1()
+
+        {
+
+            // It prints numbers from 0 to 10
+
+            for (int t = 0; t <= 10; t++)
+            {
+
+                Console.WriteLine("Thread1 is : {0}", t);
+
+                // When the value of t is equal to
+
+                // 5 then this method sleeps for
+
+                // 5 seconds and after 5 seconds
+
+                // it resumes its working
+
+                if (t == 5)
+                {
+
+                    Thread.Sleep(5000);
+
+                }
+
+            }
+
+        }
+
+        // static method two
+
+        public static void thrd2()
+
+        {
+
+            // It prints numbers from 0 to 10
+
+            for (int p = 0; p <= 10; p++)
+            {
+
+                Console.WriteLine("Thread2 is : {0}", p);
+
+            }
+
+        }
+
+    }
+
+    // Driver Class
+
+    public class mainthread2
+    {
+
+        // Main Method
+
+        static public void Main()
+
+        {
+
+            // Calling static methods
+
+            mainthread1.thrd1();
+
+            mainthread1.thrd2();
+
+        }
+
+    }
+
+}
